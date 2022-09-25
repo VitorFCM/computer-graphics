6d374e48a174b22a13ee68c4b239a8cd244215a5
VitorFCM <vitor.carreramiguel@gmail.com>
2022-09-24 21:44:01 -0300

    Corrigindo a leitura da entrada do usuario e adicionando a chamada do writeFigure

--- objects-creation/create-obj.c
+++ objects-creation/create-obj.c
@@ -3,28 +3,47 @@
 #include "../globjects.h"
 #include "triangle-creation.h"
 #include "file-structs.h"
+#include "file-manager.h"
+#include "auxiliary.h"
 
 int main(void){
 	char shape;
-	scanf("%c", &shape);
-	while(shape != '!'){
-		vertices *v;
-		objHeader *header;
-		switch(shape){
-			case 't': printf("triangulo\n");
-				  v = createTriangle(header);
-				  break;
-			case 'r': printf("retangulo\n");
-				  break;
-			case 'c': printf("circulo\n");
-				  break;
-			default:  printf("Forma desconhecida\n");
-				  break;
-		}
-		printf("Object header:\n");
-		printf("n coordenadas: %i", header->nCoordinates);
-		scanf("%c", &shape);
+	readline(&shape, 1);
+
+	vertices **shapesVertices = (vertices**)malloc(10*sizeof(vertices*));
+	objHeader **shapesHeaders = (objHeader**)malloc(10*sizeof(objHeader*));
+
+	if(shapesVertices == NULL || shapesHeaders == NULL){
+		printf("Problema no armazenamento das formas\n");
+		return 0;
 	}
+	int i = 0;
+	while(shape != '!'){
+		printf("valor de i %i\n", i);
+		vertices *v;
+		objHeader *header = (objHeader*)malloc(sizeof(objHeader));
+
+		if(shape == 't')
+			v = createTriangle(header);
+		else
+			printf("Forma desconhecida\n");
+
+		printf("Recebeu a forma\n");
+
+		shapesVertices[i] = v;
+		shapesHeaders[i++] = header;
+
+		if(i%10 == 0){
+			shapesVertices = (vertices**)realloc(shapesVertices, (10 + i)*sizeof(vertices*));
+			shapesHeaders = (objHeader**)realloc(shapesHeaders, (10 + i)*sizeof(objHeader*));
+		}
+
+		readline(&shape, 1);
+
+	}
+
+	printf("n objetos: %i\n", i);
+	writeFigure(shapesVertices, shapesHeaders, i);
 	return 0;
 }
 
